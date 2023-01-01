c6696b5a414ed3fbaa8db0e1cef56a4742403932
loclink <loclink@163.com>
2023-01-01 17:59:07 +0800

    chore: 修改刷新cdn缓存脚本逻辑，由刷新1个文件改为刷新3个

--- scripts/refresh-cdn.js
+++ scripts/refresh-cdn.js
@@ -1,4 +1,8 @@
 const axios = require('axios');
-axios.get('https://purge.jsdelivr.net/npm/oh-my-live2d/dist').then(() => {
-  console.log('cdn缓存刷新成功');
+axios.get('https://purge.jsdelivr.net/npm/oh-my-live2d/dist/index.umd.js').then(() => {
+  axios.get('https://purge.jsdelivr.net/npm/oh-my-live2d/dist/oml-cubism2.umd.js').then((res) => {
+    axios.get('https://purge.jsdelivr.net/npm/oh-my-live2d/dist/oml-cubism4.umd.js').then(() => {
+      console.log('CDN缓存刷新成功');
+    });
+  });
 });
