d7aa4f6c69bb92ac6cf47980f14c6f2c28327444
loclink <loclink@163.com>
2023-01-14 15:36:45 +0800

    build: 依赖cross-env，区分环境

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,5 @@
 /// <reference types="vite/client" />
 declare const __VERSION__: string;
+declare const __ENV__: 'env' | undefined;
 declare module 'pixi-live2d-display/cubism2';
 declare module 'pixi-live2d-display/cubism4';
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import project from './package.json';
 
+const { OML_ENV } = process.env;
 export default defineConfig({
   define: {
-    __VERSION__: JSON.stringify(project.version)
+    __VERSION__: JSON.stringify(project.version),
+    __ENV__: OML_ENV
   },
   build: {
     target: 'es6',
