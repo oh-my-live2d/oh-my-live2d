03a96b1b694a5bb70b13f39332bb145eaaeebbe4
Hacxy <hacxy.97@outlook.com>
2024-05-12 11:50:44 +0800

    refactor: add style module

--- packages/oh-my-live2d/src/modules/stage.ts
+++ packages/oh-my-live2d/src/modules/stage.ts
@@ -1,34 +1,37 @@
-import { stageStyle } from '../styles/stage.js';
+import { Style } from './style.js';
 import { createElement } from '../utils/element.js';
-import { setClassName } from '../utils/style.js';
 
 export class Stage {
   element: HTMLElement;
+  style: Style;
   constructor() {
     this.element = this.create();
+    this.style = new Style(this.element, {
+      width: '0px',
+      height: '0px',
+      position: 'fixed',
+      right: 'auto',
+      bottom: 0,
+      zIndex: '9997'
+      // transform: 'translateY(130%)'
+    });
+
+    setTimeout(() => {
+      this.style.update({
+        backgroundColor: 'rgba(0, 0, 0, 0.8)',
+        width: 100,
+        height: 100
+      });
+    }, 1000);
   }
 
   create() {
     const el = createElement({
       id: 'oml2d-stage',
-      tagName: 'div',
-      className: stageStyle({
-        width: 200,
-        height: 200,
-        backgroundColor: '#000'
-      })
+      tagName: 'div'
     });
 
     document.body.appendChild(el);
-
-    setTimeout(() => {
-      setClassName(
-        el,
-        stageStyle({
-          width: 100
-        })
-      );
-    }, 1000);
 
     return el;
   }
--- packages/oh-my-live2d/src/modules/style.ts
+++ packages/oh-my-live2d/src/modules/style.ts
@@ -1,1 +1,28 @@
-export class Style {}
+import CSS from 'csstype';
+import { css } from 'goober';
+import { mergeDeep } from 'tianjie';
+
+import { CSSProperties } from '../types/style/index.js';
+import { handleStyleProperties } from '../utils/style.js';
+
+export class Style {
+  style: CSS.Properties = {};
+  constructor(
+    private element: HTMLElement,
+    style: CSSProperties = {}
+  ) {
+    this.update(style);
+  }
+
+  update(style: CSSProperties) {
+    const styleObj = handleStyleProperties(style);
+    const finalStyle = mergeDeep(this.style, styleObj);
+
+    this.style = finalStyle;
+    const className = css({ ...finalStyle });
+
+    this.element.className = className;
+
+    return finalStyle;
+  }
+}
